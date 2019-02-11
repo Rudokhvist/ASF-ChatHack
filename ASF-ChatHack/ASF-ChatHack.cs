eee234f64d31ce8e7448f475a056ebcc0e898e39
Ryzhehvost <kotlyar.andrey@gmail.com>
2019-02-11 16:59:18 +0200

    code format applied

--- ASF-ChatHack/ASF-ChatHack.cs
+++ ASF-ChatHack/ASF-ChatHack.cs
@@ -5,16 +5,16 @@ using ArchiSteamFarm.Plugins;
 using JetBrains.Annotations;
 
 namespace ASF_ChatHack {
-    [Export(typeof(IPlugin))]
-	public sealed class ASFChatHack: IBotHackNewChat {
+	[Export(typeof(IPlugin))]
+	public sealed class ASFChatHack : IBotHackNewChat {
 
-        public string Name => nameof(ASFChatHack);
-        public Version Version => typeof(ASFChatHack).Assembly.GetName().Version;
+		public string Name => nameof(ASFChatHack);
+		public Version Version => typeof(ASFChatHack).Assembly.GetName().Version;
 
-		public bool BotUsesNewChat([NotNull] Bot bot) => bot.BotConfig.OnlineStatus!=0;
+		public bool BotUsesNewChat([NotNull] Bot bot) => bot.BotConfig.OnlineStatus != 0;
 
 		public void OnLoaded() {
-            ASF.ArchiLogger.LogGenericInfo("ASF-ChatHack Plugin by Ryzhehvost, powered by ginger cats");
-        }
-    }
+			ASF.ArchiLogger.LogGenericInfo("ASF-ChatHack Plugin by Ryzhehvost, powered by ginger cats");
+		}
+	}
 }
